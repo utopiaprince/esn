dedf4a2ccc341c5224c6380768e1e7635a04bc59
utopiaprince <gang.cheng1987@gmail.com>
2015-08-10 12:41:21 +0800

    out_modify: 添加网络协议栈部分

---
+++ code/src/stack/stack.c
@@ -0,0 +1,34 @@
+/**
+ * @brief       : 
+ *
+ * @file        : stack.c
+ * @author      : gang.cheng
+ * @version     : v0.0.1
+ * @date        : 2015/5/7
+ *
+ * Change Logs  :
+ *
+ * Date        Version      Author      Notes
+ * 2015/5/7    v0.0.1      gang.cheng    first version
+ */
+
+#include <lib.h>
+#include <prim.h>
+#include <pbuf.h>
+#include <sbuf.h>
+
+#include <stack.h>
+#include <mac.h>
+#include <nwk.h>
+#include <app.h>
+#include <hal_board.h>
+
+void stack_init(void)
+{
+    bool_t res1 = pbuf_init();
+    DBG_ASSERT(res1 != FALSE __DBG_LINE);
+    bool_t res2 = sbuf_init();
+    DBG_ASSERT(res2 != FALSE __DBG_LINE);
+
+    mac_init();
+}
