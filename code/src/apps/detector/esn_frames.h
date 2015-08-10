ee45c645bb50f251b33ad5d3f6d7e78b672e06d7
Gang.Cheng <gang.cheng1987@gmail.com>
2015-08-10 21:22:06 +0800

    in_20150810_modify: esn应用层帧定义

--- code/src/apps/detector/esn_frames.h
+++ code/src/apps/detector/esn_frames.h
@@ -14,30 +14,46 @@
 
 typedef enum
 {
-	ESN_DATA = 0,
-	ESN_CTRL = 1,
-} esn_frame_type_t;
+    ESN_DATA = 0,
+    ESN_CTRL = 1,
+} esn_frame_enum_t;
 
 typedef enum
 {
-	ALARM_N = 0,
-	ALARM_T = 1, 
-}esn_alarm_type_t;
+    ALARM_N = 0,
+    ALARM_T = 1,
+} esn_alarm_enum_t;
 
 typedef enum
 {
-	DATATYPE_VIBRATION   = 0,
-	DATATYPE_DISTANCE    = 1,
-	DATATYPE_TEMPERATURE = 2,
-	DATATYPE_PICTURE     = 3,
-} ens_frame_data_type_t;
+    DATATYPE_VIBRATION   = 0,
+    DATATYPE_DISTANCE    = 1,
+    DATATYPE_TEMPERATURE = 2,
+    DATATYPE_PICTURE     = 3,
+} ens_frame_data_enum_t;
 
 typedef enum
 {
-	PICTURE_START = 0,
-	PICTURE_DATA  = 1,
-	PICTURE_END   = 2,
-} esn_frame_picture_type_t;
+    PICTURE_START = 0,
+    PICTURE_DATA  = 1,
+    PICTURE_END   = 2,
+} esn_frame_picture_enum_t;
 
+#pragma pack(1)
+
+typedef struct
+{
+    uint8_t frame_type  : 2,
+            alarm       : 1,
+            reserved    : 5;
+} esn_frames_ctrl_t;
+
+typedef struct
+{
+    esn_frames_ctrl_t frames_ctrl;
+    uint8_t           seq;
+} esn_frames_head_t;
+
+#pragma pack()
 
 #endif
