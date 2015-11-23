e31c092b319c3b25447765d0220e104c8a1b70b7
utopiaprince <gang.cheng1987@gmail.com>
2015-11-23 09:56:38 +0800

    out_1123_modify: 修改文件格式错误；

--- code/src/apps/gateway/esn_detect.h
+++ code/src/apps/gateway/esn_detect.h
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * @brief       : this
  * @file        : esn_detect.h
  * @version     : v0.0.1
@@ -8,6 +8,22 @@
  * Date       Version     Author        Note
  * 2015-08-05  v0.0.1  gang.cheng    first version
  */
+///**
+// * @brief       : 
+// *
+// * @file        : esn_detect.h
+// * @author      : gang.cheng
+// * @version     : v0.0.1
+// * @date        : 2015/08/05
+// *
+// * Change Logs  :
+// *
+// * Date        Version      Author      Notes
+// * 2015/08/05    v0.0.1      gang.cheng    first version
+// */
+ 
+
+
 #ifndef __ESN_DETECT_H__
 #define __ESN_DETECT_H__
 
